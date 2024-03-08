054be7e0a93a4f1d551fe5b391a3d71c6f9518c2
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-03-07 16:49:09 -0800

    Update springapp.tf

--- springapp.tf
+++ springapp.tf
@@ -1,7 +1,11 @@
 resource "helm_release" "springapp" {
   name       = "springapp"
   #create_namespace = true
-  namespace  = "default"
+  namespace  = "konoha"
   repository = "https://konoha-23.github.io/konoha-springapp/"
   chart      = "springapp"
+  
+  values = [
+    "${file("springapp-values.yml")}"
+  ]
 }
